51110fe03ff2853e008f77f29522657963d9c0ab
pkboo <paulkim96@gmail.com>
2022-02-11 11:29:21 -0500

    Fix path

--- assets/grandcarnival.js
+++ assets/grandcarnival.js
@@ -1,7 +1,7 @@
 var medium = ['3L.png', '3L.png', '3L.png', '3L.png','3L.png','3I.png','3I.png','3I.png','3I.png','3I']
 var large = ['4I.png','4I.png','4Z.png','4Z.png','4L.png','4L.png', '4O.png','4O.png','4T.png', '4T']
 var giant = ['5I.png','5U.png','5offZ.png','5evenZ.png','5evenL.png','5evenL.png', '5T.png', '5+.png','5off+.png', '5offT.png', '5offO.png', 'W']
-var basepath = 'blog/images/grandcarnival/'
+var basepath = '../../images/grandcarnival/'
 
 function newQuote(players) {
     if (players == 2) {
@@ -17,21 +17,22 @@ function newQuote(players) {
 	shuffleArray(medium)
     var dismedium = medium.slice(0,players)
     dismedium.forEach(img => {
-        var image =  '<img src="' + basepath + img + '"'
+        var image =  '<img src="' + basepath + img + '"' + ' alt="">';
 	    document.getElementById('mediumDisplay').innerHTML += image
+		console.log(image)
     })
 	shuffleArray(large)
     var dislarge = large.slice(0,players)
     dislarge.forEach(img => {
-        var image =  '<img src="' + basepath + img + '"'
-	    document.getElementById('largeDisplay').innerHTML = dislarge
+        var image =  '<img src="' + basepath + img + '"' + ' alt="">';
+	    document.getElementById('largeDisplay').innerHTML += image
     })
 
 	shuffleArray(giant)
     var disgiant = giant.slice(0,players)
     disgiant.forEach(img => {
-        var image =  '<img src="' + basepath + img + '"'
-	    document.getElementById('giantDisplay').innerHTML = disgiant
+        var image =  '<img src="' + basepath + img + '"' + ' alt="">';
+	    document.getElementById('giantDisplay').innerHTML += image
     })
 
 }
